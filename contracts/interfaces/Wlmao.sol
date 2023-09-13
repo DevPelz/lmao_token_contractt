8a813773693d2ff3435916eee490151e83114b18
Dev_Pelz <pelzfx1@gmail.com>
2023-09-13 11:39:32 +0100

    add: contracts/interfaces/

---
+++ contracts/interfaces/Wlmao.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IWlmao {
+    function depositLmao(uint _amount) external;
+
+    function withdrawLmao(uint _amount) external;
+}
